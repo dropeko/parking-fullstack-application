1385fabda555b915acfaeec978af0af3eb6b785c
dropeko <pedrocoli_andrade@hotmail.com>
2025-11-20 15:50:09 -0300

    build project and correct initial identified bugs

--- src/backend/Dtos/VeiculoDtos.cs
+++ src/backend/Dtos/VeiculoDtos.cs
@@ -3,4 +3,11 @@ namespace Parking.Api.Dtos
 {
     public record VeiculoCreateDto(string Placa, string? Modelo, int? Ano, Guid ClienteId);
     public record VeiculoUpdateDto(string Placa, string? Modelo, int? Ano, Guid ClienteId);
+    public record VeiculoDto(
+        Guid Id,
+        string Placa,
+        string? Modelo,
+        int? Ano,
+        DateTime DataInclusao
+    );
 }
